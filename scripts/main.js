d188b5fb59f35091dc602d468e840052207e7b5e
ljwedin <ljwedin@gmail.com>
2021-04-23 12:51:05 +0200

    Creates successful connection to database with simple POST from loginUser.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,3 @@
 const main = document.querySelector('.main');
 
-document.getElementById('loginBtn').addEventListener('click', test);
-
-function test () {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const user = {
-        userName: username,
-        password: password
-    }
-
-    console.log(user, JSON.stringify(user));
-}
+import loginUser from './modules/loginUser.js';
--- scripts/modules/loginUser.js
+++ scripts/modules/loginUser.js
@@ -1,37 +1,82 @@
-async function authUser (user) {
-    let url = 'http://localhost:3000/login';
-    const res = fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: user// JSON.stringify(user)
-    });
-
-    return res.json();
-}
-
 function getUserInput () {
     const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const pass = document.getElementById('password').value;
 
     const user = {
         userName: username,
-        password: password
-    }
+        password: pass
+    };
 
     return user;
 }
 
-export default async function loginUser () {
-    const user = getUserInput();
-    const isValid = await authUser(user);
+export default async function login () {
+    let user = getUserInput();
+    let url = 'http://localhost:3000/login'; // https://dynwebb-01-backend.herokuapp.com/login http://localhost:3000/login
+    let response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
 
-    if (isValid) {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    };
+
+    let validUser = await response.json();
+
+    if (validUser) {
         console.log('Success!');
     } else {
         console.log('Sadface :(');
     }
 }
 
-document.getElementById('loginBtn').addEventListener('click', test);
+// async function authUser (user) {
+//     let url = 'http://localhost:3000/login'; // https://dynwebb-01-backend.herokuapp.com/login http://localhost:3000/login
+//     fetch(url, {
+//         method: 'POST',
+//         mode: 'cors',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(user)
+//     })
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+//         return data;
+//     });
+// }
+
+// function getUserInput () {
+//     const username = document.getElementById('username').value;
+//     const pass = document.getElementById('password').value;
+
+//     const user = {
+//         userName: username,
+//         password: pass
+//     };
+
+//     return user;
+// }
+
+// async function getUserCredentials () {
+//     const user = getUserInput();
+//     return isValid = await authUser(user);
+// }
+
+// export default async function loginUser () {
+//     await getUserCredentials().then((isValid) => {
+//         if (isValid) {
+//             console.log('Success!');
+//         } else {
+//             console.log('Sadface :(');
+//         }
+//     })
+// }
+
+document.getElementById('loginBtn').addEventListener('click', login);
